caf6ed23cb72456e806180331449556ff244a314
Leslie Raganit <lnraganit@gmail.com>
2023-11-16 16:14:16 -0800

    formatting

--- twilio.js
+++ twilio.js
@@ -5,8 +5,8 @@ const client = require("twilio")(accountSid, authToken);
 
 client.messages
   .create({
-    from: process.env.TWILIO_PHONE_NUMBER
+    from: process.env.TWILIO_PHONE_NUMBER,
     body: "checking",
-    to: process.env.PERSONAL_PHONE_NUMBER
+    to: process.env.PERSONAL_PHONE_NUMBER,
   })
   .then((message) => console.log(message.body));
